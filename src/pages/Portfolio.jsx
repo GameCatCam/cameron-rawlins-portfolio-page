cae5c996a61a2196eaffc4583c04074caf448181
GameCatCam <cameronrawlins11@gmail.com>
2023-11-21 21:20:01 -0500

    trying something...

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,7 +14,7 @@ export default function PortfolioPage() {
                 key={2} 
                 appLink="https://media.discordapp.net/attachments/424058611107364867/1176400725996216391/chatstrology.png?ex=656ebb8d&is=655c468d&hm=33bd2a054d01e54ec26eb12ba60549a7820f61d901c63edb19d6282ef0c9f19b&=&width=1202&height=676"
                 githubLink="https://github.com/GameCatCam/Astrology-Board"
-                appImg="../src/assets/chatstrology.png"
+                appImg={process.env.PUBLIC_URL + "/chatstrology.png"}
                 appDesc="Chatstrology (Full-Stack)"
             />
             <Project 
